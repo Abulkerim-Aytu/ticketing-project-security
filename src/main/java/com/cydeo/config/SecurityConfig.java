2af8aaacf30453bfc8575c6913019bcd182d4b49
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-09 21:55:53 +0300

    Part-1-update-2

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,16 +2,36 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 // If you are only creating a Bean,one object on other word is just basic bean. You can write it on runner class which is contain @SpringBootApplication annotation
 // If you need bounch of modification or complex beans then you better need to put it in the config calss.
 @Configuration
 public class SecurityConfig {
 
-    @Bean
+        @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        List<UserDetails> userList = new ArrayList<>();
+
+        userList.add(
+                new User("mike",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
+        );
+
+        userList.add(
+                new User("ozzy",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")))
+        );
+
+        return new InMemoryUserDetailsManager(userList);
 
     }
 }
