ed80b1071857a9c43f654df18e6b8d653ec032cc
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-10 10:49:57 +0300

    Part-1-update-6

---
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -0,0 +1,29 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.entity.User;
+import com.cydeo.entity.common.UserPrincipal;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+
+    private final UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override // Get the user from DB and convert to user Springs understands by using principal
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserNameAndIsDeleted(username,false);
+
+        if (user == null){
+            throw new UsernameNotFoundException(username);
+        }
+        return new UserPrincipal(user);
+    }
+}
