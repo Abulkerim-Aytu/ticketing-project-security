752e82e41e40d801d4eca4aef904fe3341291055
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-10 10:34:14 +0300

    Part-1-update-5

--- src/main/java/com/cydeo/entity/common/UserPrincipal.java
+++ src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -1,43 +1,52 @@
 package com.cydeo.entity.common;
 
+import com.cydeo.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
 public class UserPrincipal implements UserDetails {
+    private User user; // this user is form entity packet
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+
+
         return null;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return this.user.getPassWord();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.user.getUserName();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return this.user.isEnabled();
     }
 }
