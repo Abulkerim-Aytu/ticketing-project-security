2bfa9015d323ff5c54c12385b7cc2a3338cf0d89
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-10 16:51:13 +0300

    Part-2-update-8

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -10,7 +10,7 @@ import com.cydeo.service.ProjectService;
 import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,12 +23,14 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     private final ProjectService projectService;
     private final TaskService taskService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, @Lazy ProjectService projectService, @Lazy TaskService taskService) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, @Lazy ProjectService projectService, @Lazy TaskService taskService, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.projectService = projectService;
         this.taskService = taskService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -45,7 +47,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save(UserDTO user) {
-        userRepository.save(userMapper.convertToEntity(user));
+        User obj = userMapper.convertToEntity(user);
+
+        obj.setPassWord(passwordEncoder.encode(obj.getPassWord())); // Here we just set encode password for new created user.
+
+        userRepository.save(obj);
     }
 
 //    @Override
